3e61848f89599a6061fa2122b3a0caa45af3856d
sbuel <116324362+sbuel@users.noreply.github.com>
2025-01-25 03:52:28 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Career Recommendation App</h1>
+      <p>Welcome to your app! Start building your career recommendation tool here.</p>
+    </div>
+  );
+};
+
+export default App;
